04a86ed2058f010f382fdfa9397b5ade4e2c37a6
MineFrostID <obenunity56@gmail.com>
2023-10-18 17:18:24 +0700

    Video #5

---
+++ app/views/about/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About</title>
+</head>
+<body>
+    <h1>About Me</h1>
+    <p>Hi, my name is <?= $data['nama']; ?>. I'm <?= $data['umur']; ?> y/o, I'm <?= $data['pekerjaan']; ?>.</p>
+</body>
+</html>
