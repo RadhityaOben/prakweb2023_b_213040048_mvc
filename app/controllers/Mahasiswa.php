d59867dd5b22933287b7b644a20365bfce3dd833
MineFrostID <obenunity56@gmail.com>
2023-10-19 21:58:01 +0700

    Video #9

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -1,21 +1,33 @@
 <?php
 
-    class Mahasiswa extends Controller{
-        public function index() {
-            $data['judul'] = 'Daftar Mahasiswa';
-            $data['mhs'] = $this->model('Mahasiswa_Model')->getAllMahasiswa();
-            $this->view('templates/header', $data);
-            $this->view('mahasiswa/index', $data);
-            $this->view('templates/footer');
-        }
-
-        public function detail($id) {
-            $data['judul'] = 'Detail Mahasiswa';
-            $data['mhs'] = $this->model('Mahasiswa_Model')->getMahasiswaById($id);
-            $this->view('templates/header', $data);
-            $this->view('mahasiswa/detail', $data);
-            $this->view('templates/footer');
-        }
+class Mahasiswa extends Controller
+{
+    public function index()
+    {
+        $data['judul'] = 'Daftar Mahasiswa';
+        $data['mhs'] = $this->model('Mahasiswa_Model')->getAllMahasiswa();
+        $this->view('templates/header', $data);
+        $this->view('mahasiswa/index', $data);
+        $this->view('templates/footer');
     }
 
-?>
+    public function detail($id)
+    {
+        $data['judul'] = 'Detail Mahasiswa';
+        $data['mhs'] = $this->model('Mahasiswa_Model')->getMahasiswaById($id);
+        $this->view('templates/header', $data);
+        $this->view('mahasiswa/detail', $data);
+        $this->view('templates/footer');
+    }
+
+    public function tambah()
+    {
+        if ($this->model('Mahasiswa_Model')->tambahDataMahasiswa($_POST) > 0) {
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        } else {
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        }
+    }
+}
