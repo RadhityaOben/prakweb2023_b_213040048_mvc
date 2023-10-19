abad80c050967a51771a72d6f42384436e77d196
MineFrostID <obenunity56@gmail.com>
2023-10-19 21:21:32 +0700

    Video #8

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -8,6 +8,14 @@
             $this->view('mahasiswa/index', $data);
             $this->view('templates/footer');
         }
+
+        public function detail($id) {
+            $data['judul'] = 'Detail Mahasiswa';
+            $data['mhs'] = $this->model('Mahasiswa_Model')->getMahasiswaById($id);
+            $this->view('templates/header', $data);
+            $this->view('mahasiswa/detail', $data);
+            $this->view('templates/footer');
+        }
     }
 
 ?>
