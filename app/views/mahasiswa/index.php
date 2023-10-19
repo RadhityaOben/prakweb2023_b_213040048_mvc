b41d5165e4b53641e9d1cfc75b69d08d2090c061
MineFrostID <obenunity56@gmail.com>
2023-10-19 23:17:23 +0700

    Video #12

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -7,7 +7,7 @@
 
         <div class="row">
             <div class="col-lg-6">
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+                <button type="button" class="btn btn-primary tombolTambahData" data-bs-toggle="modal" data-bs-target="#formModal">
                     Tambah Data Mahasiswa
                 </button>
 
@@ -19,8 +19,9 @@
                     <?php foreach ($data['mhs'] as $mhs) : ?>
                         <li class="list-group-item">
                             <?= $mhs['nama']; ?>
-                            <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id'] ?>" class="btn btn-danger float-end mx-2" onclick="return confirm('Yakin?')">Hapus</a>
-                            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id'] ?>" class="btn btn-primary float-end">Detail</a>
+                            <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id'] ?>" class="btn btn-danger float-end mx-1" onclick="return confirm('Yakin?')">Hapus</a>
+                            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id'] ?>" class="btn btn-success float-end mx-1 tampilModalUbah" data-bs-toggle="modal" data-bs-target="#formModal" data-id="<?= $mhs['id']; ?>">Ubah</a>
+                            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id'] ?>" class="btn btn-primary float-end mx-1">Detail</a>
                         </li>
                     <?php endforeach; ?>
                 </ul>
@@ -29,15 +30,16 @@
         </div>
     </div>
 
-    <div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+    <div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="formModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="judulModal">Tambah Data Mahasiswa</h1>
+                    <h1 class="modal-title fs-5" id="formModalLabel">Tambah Data Mahasiswa</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="POST">
+                        <input type="hidden" name="id" id="id">
                         <div class="mb-3">
                             <label for="nama" class="form-label">Nama</label>
                             <input type="text" class="form-control" id="nama" name="nama">
@@ -53,12 +55,14 @@
                             <input type="text" class="form-control" id="email" name="email">
                         </div>
 
-                        <select class="form-select" name="jurusan" aria-label="Jurusan">
-                            <option selected>Jurusan</option>
-                            <option value="Teknik Informatika">Teknik Informatika</option>
-                            <option value="Teknik Mesin">Teknik Mesin</option>
-                            <option value="Teknologi Pangan">Teknologi Pangan</option>
-                        </select>
+                        <div class="mb-3">
+                            <label for="jurusan" class="form-label">Jurusan</label>
+                            <select class="form-select" name="jurusan" id="jurusan">
+                                <option value="Teknik Informatika">Teknik Informatika</option>
+                                <option value="Teknik Mesin">Teknik Mesin</option>
+                                <option value="Teknologi Pangan">Teknologi Pangan</option>
+                            </select>
+                        </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
